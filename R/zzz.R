68c299650d5f26c9243af2704be97537fc5b67a7
Alessandra R. Brazzale <alessandra.brazzale@unimore.it>
2009-09-14 00:00:00 +0000

    version 1.1-3

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.First.lib <- function(libname, pkgname) 
+{
+  version <- as.character("1.1-3 (2009-09-14)")
+  cat("\n   Package \"csampling\"", version, "\n")
+  cat("   Copyright (C) 2000-2009 A. R. Brazzale\n\n")
+  cat("This is free software, and you are welcome to redistribute\n")
+  cat("it and/or modify it under the terms of the GNU General\n")
+  cat("Public License published by the Free Software Foundation.\n")
+  cat("Package \"csampling\" comes with ABSOLUTELY NO WARRANTY.\n\n")
+  cat("type `help(package=\"csampling\")' for summary information\n")
+  require(marg)
+  invisible()
+}
